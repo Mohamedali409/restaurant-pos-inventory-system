96625895830aee33a1fdfa4a071477633f4f8413
Mahmoud Moselhy <130300810+mahmoudmoselhy@users.noreply.github.com>
2026-04-26 17:41:43 +0300

    Define CategorySchema for categories in MongoDB

--- src/modules/category/category.model.js
+++ src/modules/category/category.model.js
@@ -5,3 +5,29 @@
 
 // العلاقات:
 // category → products (one-to-many)
+
+
+
+import mongoose from "mongoose";
+
+
+
+const CategorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    image:{
+        type:String,
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    }
+    
+}, { timestamps: true }
+)
+
+
+module.exports= mongoose.model('Category',CategorySchema);
