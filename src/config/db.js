4ff65723dae7945216d3c230a02738b01e1c0e43
Mohamedali409 <engmohamedali409@gmail.com>
2026-04-26 16:45:52 +0300

    create server

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,24 @@
 // connectMongoDB
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "restaurantSystem",
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection Error :", error);
+    throw error;
+  }
+};
+
+export default connectDB;
