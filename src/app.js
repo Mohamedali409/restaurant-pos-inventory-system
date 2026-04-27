552d0156d25660adb58361b68f4975f6cce25026
Mohamedali409 <engmohamedali409@gmail.com>
2026-04-27 20:28:36 +0300

    create product and category from model to router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { errorHandling } from "./shared/middleware/error.middleware.js";
 import authRouter from "./modules/auth/auth.routes.js";
+import categoryRouter from "./modules/category/category.routes.js";
+import productRouter from "./modules/product/product.routes.js";
 const app = express();
 
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(cors());
 // TODO
 
 app.use("/api/auth", authRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/products", productRouter);
 
 app.use(errorHandling);
 
