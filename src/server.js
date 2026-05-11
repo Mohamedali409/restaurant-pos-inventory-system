e75316c3b78c4a425b3ee3479f2a9b7826384301
Mohamedali409 <engmohamedali409@gmail.com>
2026-05-11 20:41:50 +0300

    .

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ import "dotenv/config.js";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 // console.log(PORT);
 await connectDB();
 
-export default app;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+// export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
